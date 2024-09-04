dc5c26f9d7ef9855ddea994d5763fbe4914e2b12
Rohit kumar <126380249+Rohit20140048861@users.noreply.github.com>
2024-09-04 15:58:44 +0530

    Add files via upload

---
+++ code.php
@@ -0,0 +1,21 @@
+<!-- <?php 
+        $query = mysqli_query($db_conn,"SELECT * FROM courses ORDER BY id DESC LIMIT 0, 8");
+        while($course = mysqli_fetch_object($query))
+        {?> -->
+
+
+
+<!-- <?php echo $course->image?>
+                 -->
+
+
+                    <!-- <b><?php echo $course->name?></b> -->
+
+
+
+
+
+                           <!-- <?php echo $course->duration?> -->
+
+
+                             <!-- <?php } ?> -->
